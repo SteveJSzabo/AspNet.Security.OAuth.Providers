1af9e821a21d094285e7287d91f97494817eb790
Stephen Szabo <DOSteveSz@gmail.com>
2021-03-02 01:02:41 -0500

    Initial check-in of Bungie code
    Copied from Beam provider. Untested.

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Bungie/BungieTests.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNet.Security.OAuth.Bungie
+{
+    public class BungieTests : OAuthTests<BungieAuthenticationOptions>
+    {
+        public BungieTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => BungieAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddBungie(options => ConfigureDefaults(builder, options));
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "my-id")]
+        [InlineData(ClaimTypes.Name, "John Smith")]
+        [InlineData(ClaimTypes.Email, "john@john-smith.local")]
+        public async Task Can_Sign_In_Using_Bungie(string claimType, string claimValue)
+        {
+            // Arrange
+            using var server = CreateTestServer();
+
+            // Act
+            var claims = await AuthenticateUserAsync(server);
+
+            // Assert
+            AssertClaim(claims, claimType, claimValue);
+        }
+    }
+}
